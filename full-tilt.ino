9485eb60289c736748d270c3c6d8bf3632ef1734
Gokul Swamy <gkswamy98@gmail.com>
2017-02-18 14:51:37 -0800

    Create full-tilt.ino

---
+++ full-tilt.ino
@@ -0,0 +1,26 @@
+void setup() {
+    pinMode(A5, INPUT);
+    Particle.function("record", collectData);
+    Serial.begin(9600);
+}
+
+int collectData(String x)
+{
+    String data = "";
+    for(int i = 0; i<250; i++){
+        int point = analogRead(A5);
+        if(point > 3000){
+            data += "1";
+        }
+        else{
+            data += "0";
+        }
+        delay(120);
+    }
+    Particle.publish("new-data", data);
+    return 0;
+}
+
+void loop() {
+    
+}
